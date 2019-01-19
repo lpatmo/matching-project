2176da6555f2c3d641f34a93c7335a38e1827b0b
Angelo Cordon <angelocordon@gmail.com>
2019-01-19 12:42:21 -0800

    Update Form html and styles

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -14,36 +14,46 @@ class Form extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
   };
-  
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
-        <label>
+          <label for="name">
+            Name
+          </label>
           <input type="text" name="name" placeholder="Name" value={this.state.name} />
-        </label>
         </div>
         <div>
-        <label>
-          <input type="text" name="description" placeholder="description" value={this.state.description} />
-        </label>
+          <label for="description">
+            Description
+          </label>
+          <textarea type="textarea" name="description" placeholder="description" value={this.state.description} />
         </div>
         <div>
-        <label>
+          <label for="email">
+            Email
+          </label>
           <input type="email" name="email" placeholder="email" />
-        </label>
         </div>
-        <select name="category" value={this.state.category}>
-          <option value="mentorSignup">I want to teach/mentor</option>
-          <option value="menteeSignup">I want a mentor</option>
-          <option value="contributorSignup">
-            I want to contribute to an OS project
-          </option>
-          <option value="projectSignup">
-            I am looking for contributors to my OS project
-          </option>
-        </select>
-        <button>Submit</button>
+        <div>
+          <label for="category">
+            Category
+          </label>
+          <select name="category" value={this.state.category}>
+            <option value="mentorSignup">I want to teach/mentor</option>
+            <option value="menteeSignup">I want a mentor</option>
+            <option value="contributorSignup">
+              I want to contribute to an OS project
+            </option>
+            <option value="projectSignup">
+              I am looking for contributors to my OS project
+            </option>
+          </select>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     );
   }
